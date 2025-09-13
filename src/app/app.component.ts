62f8861bd93a6b408472451cf365001e720fee6e
mash3l <mo7amedmash3l@gmail.com>
2025-09-13 17:52:16 +0300

    second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./shared/layout/navbar/navbar.component";
 import { FooterComponent } from "./shared/layout/footer/footer.component";
 import { FloatingButtonsComponent } from './shared/floating-buttons/floating-buttons.component';
 
+declare var gtag: Function; // مهم جدًا عشان يتعرف على gtag
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent,FloatingButtonsComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, FloatingButtonsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Samy Mahrous';
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        gtag('config', 'AW-17542736996', {
+          page_path: event.urlAfterRedirects,
+        });
+      }
+    });
+  }
 }
